ad6c60b74cac720f17320f708becdc6b6dec664b
Robin Gregorio <robindanilo2218@gmail.com>
2026-09-02 08:10:06 -0500

    reparacion de notas

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Sube el numero de VERSION cada vez que cambies index.html o las plantillas:
    asi el navegador se entera de que hay algo nuevo.
    ========================================================================== */
-var VERSION = "v82";
+var VERSION = "v86";
 var CACHE = "mdcrgm-" + VERSION;
 
 var CONCHA = [
